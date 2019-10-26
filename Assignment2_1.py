d8eda7c63326ee4a971faf200f3523683ada6793
tush922 <57027524+tush922@users.noreply.github.com>
2019-10-26 22:35:59 +0530

    Add files via upload

---
+++ Assignment2_1.py
@@ -0,0 +1,19 @@
+def add(num1,num2):
+    addition=num1+num2;
+    return addition;
+
+
+def sub(num1,num2):
+    substraction=num1-num2;
+    return substraction;
+
+
+def mult(num1,num2):
+    multiplication=num1*num2;
+    return multiplication;
+
+
+def div(num1,num2):
+    division=num1/num2;
+    return division;
+
