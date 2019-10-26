a308f54e19f25d7a29011aa1adaea948077b9f01
tush922 <57027524+tush922@users.noreply.github.com>
2019-10-26 11:39:01 +0530

    Add files via upload

---
+++ additionofnumbers.py
@@ -0,0 +1,13 @@
+def add(number1,number2):
+    addition = number1+number2;
+    print("Additon is ",addition);
+
+
+print("Addition of two numbers");
+print("Enter first number");
+num1=int(input());
+print("Enter second number");
+num2=int(input());
+add(num1,num2);
+
+
