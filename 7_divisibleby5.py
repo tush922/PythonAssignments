4db3e3f34c579948f4d644e886e45631df2f4d11
tush922 <57027524+tush922@users.noreply.github.com>
2019-10-26 12:31:51 +0530

    Add files via upload

---
+++ 7_divisibleby5.py
@@ -0,0 +1,11 @@
+def divisible(num):
+    print("divisible by 5 ?")
+    if num%5==0:
+        print("True");
+    else:
+        print("False")
+
+
+print("Enter number")
+num=int(input());
+divisible(num)
